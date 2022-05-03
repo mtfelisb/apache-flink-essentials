1be3ff2d50562ee6c4398705069e3ba542f46559
Matheus Felisberto <matheus.felisberto@hotmail.com>
2022-05-02 21:52:50 -0300

    add(generators): gaming generator

---
+++ src/main/scala/Generators/Gaming.scala
@@ -0,0 +1,92 @@
+package Generators
+
+import java.time.Instant
+import java.util.UUID
+import scala.concurrent.duration.FiniteDuration
+
+package object Gaming {
+  sealed trait ServerEvent {
+    def eventTime: Instant
+    def getId: String
+  }
+
+  sealed trait GameType
+  case object OneVsOne extends GameType
+  case object TwoVsTwo extends GameType
+  case object ThreeVsThree extends GameType
+  case object FourVsFour extends GameType
+
+  // game events
+  final case class GameStarted(
+    eventTime: Instant,
+    gameId: UUID,
+    playerIds: Vector[UUID],
+    mapId: String,
+    regionId: String,
+    gameType: GameType
+  ) extends ServerEvent {
+    override def getId: String = s"game|$gameId"
+  }
+
+  final case class GameFinished(
+     eventTime: Instant,
+     gameId: UUID
+  ) extends ServerEvent {
+    override def getId: String = s"game|$gameId"
+  }
+
+  final case class PlayerRegistered(
+     eventTime: Instant,
+     playerId: UUID,
+     nickname: String
+  ) extends ServerEvent {
+    override def getId: String = s"player|$playerId|$nickname"
+  }
+
+  final case class PlayerOnline(
+    eventTime: Instant,
+    playerId: UUID,
+    nickname: String
+  ) extends ServerEvent {
+    override def getId: String = s"player|$playerId|$nickname"
+  }
+
+  final case class PlayerIsLookingForAGame(
+    eventTime: Instant,
+    playerId: UUID,
+    gameType: GameType
+  ) extends ServerEvent {
+    override def getId: String = s"player|$playerId"
+  }
+
+  final case class PlayerOffline(
+    eventTime: Instant,
+    playerId: UUID,
+    nickname: String
+  ) extends ServerEvent {
+    override def getId: String = s"player|$playerId|$nickname"
+  }
+
+  // players actions
+  case class Player(playerId: UUID, nickname: String) {
+    def register(d: FiniteDuration)(implicit startTime: Instant): PlayerRegistered =
+      PlayerRegistered(startTime.plusMillis(d.toMillis), playerId, nickname)
+
+    def online(d: FiniteDuration)(implicit startTime: Instant): PlayerOnline =
+      PlayerOnline(startTime.plusMillis(d.toMillis), playerId, nickname)
+
+    def offline(d: FiniteDuration)(implicit startTime: Instant): PlayerOffline =
+      PlayerOffline(startTime.plusMillis(d.toMillis), playerId, nickname)
+
+    def lookingForAGame(startTime: Instant, d: FiniteDuration, gameType: GameType): PlayerIsLookingForAGame =
+      PlayerIsLookingForAGame(startTime.plusMillis(d.toMillis), playerId, gameType)
+  }
+
+  // some mocked players
+  val bob: Player = Player(UUID.randomUUID(), "bob")
+  val sam: Player = Player(UUID.randomUUID(), "sam")
+  val rob: Player = Player(UUID.randomUUID(), "rob")
+  val alice: Player = Player(UUID.randomUUID(), "alice")
+  val mary: Player = Player(UUID.randomUUID(), "mary")
+  val carl: Player = Player(UUID.randomUUID(), "carl")
+}
